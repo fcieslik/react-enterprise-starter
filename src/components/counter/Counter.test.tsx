4ea79cf2c465b9380db883138c13b26e32216243
Filip Cieslik <filip.cieslik@vattenfall.com>
2023-06-20 11:45:45 +0200

    refactor: prettier fix

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -1,13 +1,13 @@
-import Counter from "./Counter";
-import { describe, test, expect } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import Counter from './Counter';
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 describe('<Counter />', () => {
-    test('Counter mounts properly', () => {
-        const counter = render(<Counter />)
-        expect(counter).toBeTruthy()
+  test('Counter mounts properly', () => {
+    const counter = render(<Counter />);
+    expect(counter).toBeTruthy();
 
-        const currentCount = screen.getByTestId('current-count');
-        expect(currentCount).toHaveTextContent('0');
-    })
+    const currentCount = screen.getByTestId('current-count');
+    expect(currentCount).toHaveTextContent('0');
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
-    setupFiles: ['./tests/setup.ts']
+    setupFiles: ['./tests/setup.ts'],
   },
 });
